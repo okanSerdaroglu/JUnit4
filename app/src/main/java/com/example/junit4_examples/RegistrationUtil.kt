9307072fd3db698c32063481ca4106b8e003c817
okanSerdaroglu <okanserdaroglu2@gmail.com>
2021-02-21 22:26:41 +0300

    validateRegistrationInput method

--- app/src/main/java/com/example/junit4_examples/RegistrationUtil.kt
+++ app/src/main/java/com/example/junit4_examples/RegistrationUtil.kt
@@ -11,13 +11,29 @@ object RegistrationUtil {
      * .. the password contains less then 2 digits
      */
 
-    private val existingUsers = listOf("Peter","Carl")
+    private val existingUsers = listOf("Peter", "Carl")
 
     fun validateRegistrationInput(
         username: String,
         password: String,
         confirmedPassword: String
     ): Boolean {
+        if (username.isEmpty() || password.isEmpty()) {
+            return false
+        }
+
+        if (username in existingUsers) {
+            return false
+        }
+
+        if (password != confirmedPassword) {
+            return false
+        }
+
+        if (password.count { it.isDigit() } < 2) {
+            return false
+        }
+
         return true
     }
 
